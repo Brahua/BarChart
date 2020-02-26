f70d2f7a277f9e555f5fef9ec3e14deaa18c7606
josue bravo <josuebh62@gmail.com>
2020-02-26 10:21:35 -0500

    .

--- BarChart/MacawChartView.swift
+++ BarChart/MacawChartView.swift
@@ -11,28 +11,29 @@ import Macaw
 
 class MacawChartView: MacawView {
     
-    static let lastFiveShows = createDummyData()
-    static let maxValue = 6000
-    static let maxValueLineHeight = 180
-    static let lineWidth: Double = 275
-    static let dataDivisor = Double(maxValue/maxValueLineHeight)
-    static let adjustedData: [Double] = lastFiveShows.map { $0.budget / dataDivisor}
-    static var animations: [Animation] = []
+    static let lastFiveShows            = createDummyData()
+    static let maxValue                 = 6000
+    static let maxValueLineHeight       = 180
+    static let lineWidth: Double        = 275
+    static let dataDivisor              = Double(maxValue/maxValueLineHeight)
+    static let adjustedData: [Double]   = lastFiveShows.map({ $0.budget / dataDivisor})
+    static var animations: [Animation]  = []
     
-    required init(coder aDecoder: NSCoder) {
-        super.init(node: MacawChartView.createChart(), coder: aDecoder)!
+    required init?(coder aDecoder: NSCoder) {
+        super.init(node: MacawChartView.createChart(), coder: aDecoder)
         backgroundColor = .clear
     }
     
     private static func createChart() -> Group {
-        var items: [Node] = addYAxisItems() + addYAxisItems()
+        var items: [Node] = addYAxisItems() + addXAxisItems()
         items.append(createBars())
+        
         return Group(contents: items, place: .identity)
     }
     
     private static func addYAxisItems() -> [Node] {
-        let maxLines = 6
-        let lineInterval = Int(maxValue/maxLines)
+        let maxLines            = 6
+        let lineInterval        = Int(maxValue/maxLines)
         let yAxisHeight: Double = 200
         let lineSpacing: Double = 30
         
@@ -41,9 +42,9 @@ class MacawChartView: MacawView {
         for i in 1...maxLines {
             let y = yAxisHeight - (Double(i) * lineSpacing)
             
-            let valueLine = Line(x1: -5, y1: y, x2: lineWidth, y2: y).stroke(fill: Color.white.with(a: 0.10))
-            let valueText = Text(text: "\(i*lineInterval)", align: .max, baseline: .mid, place: .move(dx: -10, dy: y))
-            valueText.fill = Color.white
+            let valueLine   = Line(x1: -5, y1: y, x2: lineWidth, y2: y).stroke(fill: Color.white.with(a: 0.10))
+            let valueText   = Text(text: "\(i*lineInterval)", align: .max, baseline: .mid, place: .move(dx: -10, dy: y))
+            valueText.fill  = Color.white
             
             newNodes.append(valueLine)
             newNodes.append(valueText)
@@ -51,6 +52,7 @@ class MacawChartView: MacawView {
         
         let yAxis = Line(x1: 0, y1: 0, x2: 0, y2: yAxisHeight).stroke(fill: Color.white.with(a: 0.25))
         newNodes.append(yAxis)
+        
         return newNodes
     }
     
@@ -60,8 +62,10 @@ class MacawChartView: MacawView {
         
         for i in 1...adjustedData.count {
             let x = (Double(i)*50)
+            
             let valueText = Text(text: lastFiveShows[i-1].month, align: .max, baseline: .mid, place: .move(dx: x, dy: chartBaseY + 15))
             valueText.fill = Color.white
+            
             newNodes.append(valueText)
         }
         
@@ -72,13 +76,13 @@ class MacawChartView: MacawView {
     }
     
     private static func createBars() -> Group {
-        let fill = LinearGradient(degree: 90, from: Color(val: 0xff4704), to: Color(val: 0xff4704).with(a: 0.33))
-        let items = adjustedData.map{_ in Group()}
+        let fill    = LinearGradient(degree: 90, from: Color(val: 0xff4704), to: Color(val: 0xff4704).with(a: 0.33))
+        let items   = adjustedData.map{_ in Group()}
         
         animations = items.enumerated().map{ (i: Int, item: Group) in
             item.contentsVar.animation(delay: Double(i) * 0.1) { t in
-                let height = adjustedData[i] * t
-                let rect = Rect(x: Double(i) * 50 + 25, y: 200 - height, w: 30, h: height)
+                let height  = adjustedData[i] * t
+                let rect    = Rect(x: Double(i) * 50 + 25, y: 200 - height, w: 30, h: height)
                 return [rect.fill(with: fill)]
             }
         }
@@ -91,11 +95,11 @@ class MacawChartView: MacawView {
     }
     
     private static func createDummyData() -> [Budget] {
-        let one = Budget(month: "Enero", budget: 3200)
-        let two = Budget(month: "Enero", budget: 4200)
-        let three = Budget(month: "Enero", budget: 5200)
-        let four = Budget(month: "Enero", budget: 3800)
-        let five = Budget(month: "Enero", budget: 3990)
+        let one     = Budget(month: "1", budget: 3200)
+        let two     = Budget(month: "2", budget: 4200)
+        let three   = Budget(month: "3", budget: 5200)
+        let four    = Budget(month: "4", budget: 3800)
+        let five    = Budget(month: "5", budget: 3990)
         
         return [one, two, three, four, five]
     }
