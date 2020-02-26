d0e574c07b71daef53a272cf7550ea9be96a0997
Jerlouis Vivas <jerlouisvivas@gmail.com>
2020-02-26 01:40:27 -0500

    Initial Commit

---
+++ BarChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarChart
+//
+//  Created by MacSpace on 2/26/20.
+//  Copyright © 2020 Brahua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
